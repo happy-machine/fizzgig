63c6aeda980e6dd1718c2f053df25fdfd02c9697
Dan Stein <33182553+happy-machine@users.noreply.github.com>
2020-04-25 16:05:46 +0100

    Update tickers.js

--- serverless/controllers/tickers.js
+++ serverless/controllers/tickers.js
@@ -3,7 +3,6 @@ const { getAsync } = require("../services/elasticache");
 async function run(symbols) {
   const parsedSymbols = JSON.parse(symbols);
   const symbolsValues = parsedSymbols.symbols;
-  try {
     if (!Array.isArray(symbolsValues)) {
       return {
         symbol: symbolsValues,
@@ -12,13 +11,9 @@ async function run(symbols) {
     }
     const blob = await Promise.all(fetchSymbolsFromRedis(symbolsValues));
     return blob;
-  } catch (e) {
-    Promise.reject(new Error(e));
-  }
 }
 
 const fetchSymbolsFromRedis = (symbols) => {
-  console.log("in fetch symbols with ", symbols, Array.isArray(symbols));
   return symbols.map(async (symbol) => ({
     symbol,
     stockValue: await getAsync(symbol),
