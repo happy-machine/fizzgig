54ef528be9a644fb94cefd84345c9a80e3349869
Dan Stein <33182553+happy-machine@users.noreply.github.com>
2020-04-25 15:59:45 +0100

    Update search.js

--- serverless/controllers/search.js
+++ serverless/controllers/search.js
@@ -10,15 +10,11 @@ const searchHttp = rateLimit(axios.create(), {
 // temporary debounce hack due to time restraints!
 
 async function search(keywords) {
-  try {
     const response = await searchHttp.get(
       `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keywords}&apikey=${selectAlphaVantageKey()}`
     );
     console.log("in response with: ", response.data);
     return response.data;
-  } catch (e) {
-    throw new Error(e);
-  }
 }
 
 module.exports = {
