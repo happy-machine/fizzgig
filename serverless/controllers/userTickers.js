def467bb3a3b84f90e432ada710bab5b52731915
Dan Stein <33182553+happy-machine@users.noreply.github.com>
2020-04-25 16:04:20 +0100

    Update userTickers.js

--- serverless/controllers/userTickers.js
+++ serverless/controllers/userTickers.js
@@ -1,24 +1,15 @@
 const User = require("../models/User");
 
 async function getUser(userId) {
-  try {
     const user = await User.findById(userId, { password: 0 });
     return !user ? Promise.reject("No user found.") : user;
-  } catch (e) {
-    return Promise.reject(new Error(e));
-  }
 }
 
 async function updateUserTickers(userId, tickers) {
-  try {
     console.log(`updating ${userId} with ${tickers}`);
     await User.updateOne({ _id: userId }, { tickers });
     const user = await User.findById(userId);
     return user;
-  } catch (e) {
-    console.log("error in update");
-    return Promise.reject(new Error(e));
-  }
 }
 
 module.exports = {
